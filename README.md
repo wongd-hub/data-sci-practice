c8fbe62ec5edf714d8674c0d55954339be4cf9d8
Darren Wong <email@darrenwong.net>
2022-05-22 11:57:28 +1000

    Updated README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Kaggle Analyses Repository
+# Data Science Practice Repository
 
-This is a repository I'm using to store my Kaggle Analyses which I'm doing to practice my data analysis skills and to learn more about and practice machine learning.
+This is a repository I'm using to store my data science (including Kaggle) analyses which I'm doing to practice my data science skills and to learn more about and practice machine learning.
 
-- [**Spaceship Titanic**](./spaceship-titanic): Building a binary classifier (random forest & XGBoost) to determine whether someone will be inadvertently teleported to an alternate dimension.
-- [**Favorita Sales**](./favorita-sales): Forecasting the sales volumes of thousands of items at Favorita stores in Ecuador using time series analysis.
+- [**Kaggle - Spaceship Titanic**](./spaceship-titanic): Building a binary classifier (random forest & XGBoost) to determine whether someone will be inadvertently teleported to an alternate dimension.
+- [**Kaggle - Favorita Sales**](./favorita-sales): Forecasting the sales volumes of thousands of items at Favorita stores in Ecuador using time series analysis.
--- favorita-sales/initial_analysis.R
+++ favorita-sales/initial_analysis.R
@@ -4,9 +4,8 @@ invisible({
   # Packages to load are found in the character vector below
   packages <- c(
     "tidyverse", "data.table", "ggplot2", 
-    "scales", "tictoc", "infotheo", "ROCR",
-    "Information", "gridExtra", "mice",
-    "randomForest", "caret", "xgboost"
+    "scales", "tictoc", "fable", "tsibble",
+    "seasonal", "feasts"
   )
   
   if(!all(packages %in% rownames(installed.packages()))) {
@@ -27,3 +26,59 @@ if (.Platform$OS.type == "windows") {
 } else {
   setwd('~/Documents/Projects/kaggle/favorita-sales') # Macbook
 }
+
+## Load data & manipulate ----
+train <- fread('raw-data/train.csv') %>% as_tibble()
+test  <- fread('raw-data/test.csv') %>% as_tibble()
+
+holidays_events <- fread('raw-data/holidays_events.csv') %>% as_tibble()
+transactions    <- fread('raw-data/transactions.csv') %>% as_tibble()
+oil_prices      <- fread('raw-data/oil.csv') %>% as_tibble()
+stores          <- fread('raw-data/stores.csv') %>% as_tibble()
+
+## 1a. EDA - Time series ----
+train_ts <- train %>% 
+  select(-id) %>% 
+  as_tsibble(
+    index = date,
+    key = c(store_nbr, family)
+  )
+
+train_ts_agg <- train %>% 
+  group_by(date, family) %>% 
+  summarise(sales = sum(sales, na.rm = TRUE), .groups = 'drop') %>% 
+  as_tsibble(
+    index = date, key = family
+  ) %>% 
+  group_by_key() %>% 
+  index_by(year_month = ~ yearmonth(.)) %>% 
+  summarise(sales = sum(sales, na.rm = TRUE), .groups = 'drop')
+
+train_ts %>% 
+  index_by(vars(date, family)) %>% 
+  summarise(sales = sum(sales, na.rm = TRUE))
+  update_tsibble(key = c(family)) %>%
+  group_by_key() %>% 
+  summarise(sales = sum(sales, na.rm = TRUE)) %>% 
+  # filter(family == 'AUTOMOTIVE') %>% 
+  ggplot(aes(x = date, y = sales, colour = family)) +
+  geom_line() +
+  facet_wrap(vars(family), scales = 'free') +
+  theme(legend.position = 'none')
+
+train_ts %>% 
+  group_by_key() %>% 
+  index_by(year_month = ~ yearmonth(.)) %>% 
+  summarise(sales = sum(sales, na.rm = TRUE), .groups = 'drop') %>% 
+  ggplot(aes(x = year_month, y = sales, colour = family)) +
+  facet_wrap(vars(family), scales = 'free') +
+  geom_line() +
+  theme(legend.position = 'none')
+
+## 1b. EDA - Auxilliary datasets ----
+
+
+
+
+
+## 
